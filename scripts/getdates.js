44c2807a99e3956bf8e00c71a5bd2d3bde08a392
benjaminihentuge <benjaminihentuge2017@gmail.com>
2026-03-15 00:40:46 +0100

    Add initial project files including HTML, CSS, JavaScript, and images
    - Created index.html with structure and content for the project.
    - Added styles for small and large screens in small.css and large.css.
    - Implemented JavaScript functionality for dynamic date display and navigation toggle in getdates.js.
    - Included various image assets: byui_Logo.svg, download.png, me2.jpeg, and profile-pic.webp.

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent =
+  `Last modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
